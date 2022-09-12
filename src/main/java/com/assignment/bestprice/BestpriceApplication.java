dd079e7a1b6f3087a4bf29f8f56c97ea9b4c6e85
akhilr <akhilrdd7@gmail.com>
2022-09-11 22:53:18 -0500

    Completed assignment

---
+++ src/main/java/com/assignment/bestprice/BestpriceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.bestprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestpriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestpriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/bestprice/BestpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.bestprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestpriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
